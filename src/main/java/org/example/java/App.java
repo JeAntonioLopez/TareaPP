9880df535b2304100b42c6a64aca6eed80cfc91c
Jean Lopez <jeanlopezcortes@gmail.com>
2023-10-01 21:59:02 -0300

    Singleton

--- src/main/java/org/example/java/App.java
+++ src/main/java/org/example/java/App.java
@@ -1,50 +1,34 @@
 package org.example.java;
 
-import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class App {
+
     public static void main(String[] args) {
-        Connection connection = null;
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+
+        String insertQuery1 = "INSERT INTO usuarios (nombre, edad) VALUES ('Ejemplo1', 25)";
+        String insertQuery2 = "INSERT INTO usuarios (nombre, edad) VALUES ('jeanlopez', 30)";
+        databaseManager.insertData(insertQuery1);
+        databaseManager.insertData(insertQuery2);
+
+        System.out.println("Sample data inserted into the 'usuarios' table.");
+
+        ResultSet resultSet = databaseManager.executeQuery("SELECT * FROM usuarios");
+
         try {
-
-            connection = DriverManager.getConnection("jdbc:sqlite:/home/felipe/base_datos_prueba.db");
-
-
-            Statement statement = connection.createStatement();
-
-
-            String insertQuery1 = "INSERT INTO usuarios (nombre, edad) VALUES ('Ejemplo1', 25)";
-            String insertQuery2 = "INSERT INTO usuarios (nombre, edad) VALUES ('jeanlopez', 30)";
-            statement.executeUpdate(insertQuery1);
-            statement.executeUpdate(insertQuery2);
-
-            System.out.println("Sample data inserted into the 'usuarios' table.");
-
-
-            ResultSet resultSet = statement.executeQuery("SELECT * FROM usuarios");
-
             while (resultSet.next()) {
-
                 int id = resultSet.getInt("id");
                 String nombre = resultSet.getString("nombre");
                 int edad = resultSet.getInt("edad");
 
-
                 System.out.println("ID: " + id + ", Nombre: " + nombre + ", Edad: " + edad);
             }
-
         } catch (SQLException e) {
-
             e.printStackTrace();
         } finally {
-            try {
-
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            databaseManager.closeConnection();
         }
     }
 }
---
+++ src/main/java/org/example/java/DatabaseManager.java
@@ -0,0 +1,54 @@
+package org.example.java;
+
+import java.sql.*;
+
+public class DatabaseManager {
+
+    private static DatabaseManager instance;
+    private Connection connection;
+
+    private DatabaseManager() {
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\jeanl\\OneDrive\\Documents\\GitHub\\TareaPP\\src\\main\\java\\org\\example\\java\\base_datos_prueba.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static DatabaseManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
+
+    public void insertData(String query) {
+        try {
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet executeQuery(String query) {
+        ResultSet resultSet = null;
+        try {
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
